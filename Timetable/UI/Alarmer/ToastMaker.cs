eb4802179113b5510f0ad0585e831f1e0ede69c2
TamasRichter <tamas.richter.91@gmail.com>
2018-03-13 10:32:47 +0100

    add toastmaker for notification

---
+++ Timetable/UI/Alarmer/ToastMaker.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.QueryStringDotNET;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.Models;
+
+namespace UI.Alarmer
+{
+    public static class ToastMaker
+    {
+        public static ToastVisual GetVisual(Bus bus)
+        {
+            return new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = "Dude, Where's My Bus?"
+                        },
+
+                        new AdaptiveText()
+                        {
+                            Text = bus.Departing
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
